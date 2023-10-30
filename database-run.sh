82e5bd4b114ff56508887525659267a8e05c63df
itsmegb <itsmegb@users.noreply.github.com>
2023-10-30 10:37:30 +0000

    use correct file path for deployment files (#10)

--- database-run.sh
+++ database-run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 #  Create db configmap
-kubectl apply -f database/postgresql-configmap.yaml
-
+kubectl apply -f tracey-database/postgresql-configmap.yaml
 
 #  Create db persistant volume chain
-kubectl apply -f database/postgresql-pvc.yaml
+kubectl apply -f tracey-database/postgresql-pvc.yaml
 
 #  Create db deployment
-kubectl apply -f database/postgresql-deployment.yaml
+kubectl apply -f tracey-database/postgresql-deployment.yaml
 
 #  Create db service
-kubectl apply -f database/postgresql-service.yaml
+kubectl apply -f tracey-database/postgresql-service.yaml
