18bf44ca49c1d339fd824773b04bc3b8c2e05402
georgep1ckers <george.pickers@hotmail.co.uk>
2023-11-01 15:13:04 +0000

    combining service and deployment yamls

--- tracey-run.sh
+++ tracey-run.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-#  Create tracey stock-controller deployment
-kubectl apply -f stock-controller-deployment.yaml
+#  Create tracey stock-controller deployment & service combined yaml
+kubectl apply -f stock-controller.yaml
 
-#  Create tracey stock-controller service
-kubectl apply -f stock-controller-service.yaml
+#  Create tracey processor deployment & service combined yaml
+kubectl apply -f order-processor.yaml
 
-#  Create tracey processor deployment
-kubectl apply -f order-processor-deployment.yaml
-
-#  Create tracey processor service
-kubectl apply -f order-processor-service.yaml
-
-#  Create tracey interface deployment
-kubectl apply -f warehouse-interface-deployment.yaml
+#  Create tracey interface deployment yaml
+kubectl apply -f warehouse-interface.yaml
